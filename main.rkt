bcf51f2c595e40ca66330bbccc895afb852a6292
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-02-28 14:13:11 -0600

    update

--- main.rkt
+++ main.rkt
@@ -1,10 +1,10 @@
 #lang racket
 (define length2
   (λ (1st)
-    (printf "~a~n" 1st)
-    (if (null? 1st)
-      0
-      (+ 1 (length2 (cdr 1st))))))
+         (printf "~a~n" 1st)
+         (if (null? 1st)
+             0
+             (+ 1 (length2 (cdr 1st))))))
 			
 
 
@@ -12,9 +12,9 @@
   (λ (1st)
     (define length-acc-helper
       (λ (1st acc)
-	      (printf "1st=~a acc=~a~n" 1st acc)
+	(printf "1st=~a acc=~a~n" 1st acc)
         (if (empty? 1st)
-          acc
-          (length-acc-helper (cdr 1st) (+ 1 acc)))))
-  (length-acc-helper 1st 0)))
+            acc
+            (length-acc-helper (cdr 1st) (+ 1 acc)))))
+    (length-acc-helper 1st 0)))
 			
