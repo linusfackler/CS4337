4dbe5bb0c3c342409e099896b9e8658f283d4d08
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-03-11 21:45:50 -0600

    update

--- Assignment 1/lxf210001.rkt
+++ Assignment 1/lxf210001.rkt
@@ -1,2 +1,10 @@
 #lang racket
-(provide (alldefined-out))
+;(provide (alldefined-out))
+
+;(define (greet name)
+;  (string-append salutation ", " name))
+
+(define ((divisible-by-x? x) y) 
+  (if (= 0 (modulo y x))
+         "#t"
+         "$f"))
